1e02212a13f73a7b5bd0d3be75851473c57e1104
nirupavi <104073745+nirupavi@users.noreply.github.com>
2022-05-27 19:01:13 +0530

    Add files via upload

--- CVapp/admin.py
+++ CVapp/admin.py
@@ -5,6 +5,8 @@ from .models import Resume
 @admin.register(Resume)
 
 class ResumeAdmin(admin.ModelAdmin):
-    list_display = ['id','name','email','mobile','locality','city','pin','state','School','School_marks',
-                    'Degree','University','Degree_percentage','previous_roll','previous_experience',
-                    'Tech_skill','profile_image','file']
+    pass
+    #list_display = ['id','name','email','mobile','locality','city','pin','state','School','School_marks','Degree','University','Degree_percentage','previous_roll','previous_experience','Tech_skill','profile_image','file']
+
+
+
