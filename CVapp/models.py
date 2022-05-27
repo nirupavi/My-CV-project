ec3dd1ab3637a18e0cfef478164d512cec874d01
nirupavi <104073745+nirupavi@users.noreply.github.com>
2022-05-27 19:00:41 +0530

    Add files via upload

--- CVapp/models.py
+++ CVapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Resume(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     email = models.EmailField(max_length=100)
     mobile = models.PositiveIntegerField()
     locality = models.CharField(max_length=100)
@@ -24,3 +24,6 @@ class Resume(models.Model):
     profile_image = models.ImageField(upload_to='profileimg', blank=True)
     file = models.FileField(upload_to='doc',blank=True)
 
+
+    def __str__(self):
+        return self.name
